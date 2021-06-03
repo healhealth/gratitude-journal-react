58f31ddaa8d982104fb2da85474ca586e5d8e21e
Bence A. Tóth <tothab@gmail.com>
2021-06-03 21:34:33 +0200

    Added some blank lines

--- src/components/PageForm/PageForm.js
+++ src/components/PageForm/PageForm.js
@@ -2,6 +2,7 @@ const PageForm = () => {
   return (
     <div className="PageForm">
       <h2>Morning section</h2>
+
       <h3>3 things I am grateful for today</h3>
       <p>I am grateful for</p>
       <textarea></textarea>
@@ -9,6 +10,7 @@ const PageForm = () => {
       <textarea></textarea>
       <p>I am grateful for</p>
       <textarea></textarea>
+
       <h3>3 things that would make today great</h3>
       <p>Today would be great if</p>
       <textarea></textarea>
@@ -16,10 +18,13 @@ const PageForm = () => {
       <textarea></textarea>
       <p>Today would be great if</p>
       <textarea></textarea>
+
       <h3>Daily affirmation</h3>
       <p>I am</p>
       <textarea></textarea>
+
       <h2>Evening section</h2>
+
       <h3>List 3 things that made your day amazing</h3>
       <p>What made today amazing was</p>
       <textarea></textarea>
@@ -27,6 +32,7 @@ const PageForm = () => {
       <textarea></textarea>
       <p>What made today amazing was</p>
       <textarea></textarea>
+
       <h3>How could I have made today even better?</h3>
       <p>Today would have been even better if</p>
       <textarea></textarea>
