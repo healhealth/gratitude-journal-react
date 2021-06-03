10e628d872a618a75065b0ec4cc9ac2b4facdc96
Bence A. Tóth <tothab@gmail.com>
2021-06-03 21:33:53 +0200

    Added HTML form structure

--- src/components/PageForm/PageForm.js
+++ src/components/PageForm/PageForm.js
@@ -1,21 +1,35 @@
 const PageForm = () => {
   return (
     <div className="PageForm">
-      <p>Morning section</p>
-      <p>3 things I am grateful for today (I am grateful for ______)</p>
-      <p>
-        3 things that would make today great (Today would be great if ______)
-      </p>
-      <p>Daily affirmation (I am ______)</p>
-      <p>Evening section</p>
-      <p>
-        List 3 things that made your day amazing (What made today amazing was
-        ______)
-      </p>
-      <p>
-        How could I have made today even better? (Today would have been even
-        better if ______)
-      </p>
+      <h2>Morning section</h2>
+      <h3>3 things I am grateful for today</h3>
+      <p>I am grateful for</p>
+      <textarea></textarea>
+      <p>I am grateful for</p>
+      <textarea></textarea>
+      <p>I am grateful for</p>
+      <textarea></textarea>
+      <h3>3 things that would make today great</h3>
+      <p>Today would be great if</p>
+      <textarea></textarea>
+      <p>Today would be great if</p>
+      <textarea></textarea>
+      <p>Today would be great if</p>
+      <textarea></textarea>
+      <h3>Daily affirmation</h3>
+      <p>I am</p>
+      <textarea></textarea>
+      <h2>Evening section</h2>
+      <h3>List 3 things that made your day amazing</h3>
+      <p>What made today amazing was</p>
+      <textarea></textarea>
+      <p>What made today amazing was</p>
+      <textarea></textarea>
+      <p>What made today amazing was</p>
+      <textarea></textarea>
+      <h3>How could I have made today even better?</h3>
+      <p>Today would have been even better if</p>
+      <textarea></textarea>
     </div>
   );
 };
