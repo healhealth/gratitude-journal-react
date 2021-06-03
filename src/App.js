54b1f8282755c21626133275f85468cce1807380
Bence A. Tóth <tothab@gmail.com>
2021-06-03 22:40:22 +0200

    Set grid on PageForm component, set height of sections and set color of evening section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import "./App.css";
 import PageForm from "./components/PageForm/PageForm";
+
+import "./App.css";
 
 const App = () => {
   return (
--- src/components/PageForm/PageForm.js
+++ src/components/PageForm/PageForm.js
@@ -1,32 +1,37 @@
 import PageFormItem from "./PageFormItem/PageFormItem";
 
+import "./PageForm.css";
+
 const PageForm = () => {
   return (
     <div className="PageForm">
-      <h2>Morning section</h2>
+      <div className="MorningForm">
+        <h2>Morning section</h2>
 
-      <h3>3 things I am grateful for today</h3>
-      <PageFormItem leadText="Today I am grateful for" />
-      <PageFormItem leadText="Today I am grateful for" />
-      <PageFormItem leadText="Today I am grateful for" />
+        <h3>3 things I am grateful for today</h3>
+        <PageFormItem leadText="Today I am grateful for" />
+        <PageFormItem leadText="Today I am grateful for" />
+        <PageFormItem leadText="Today I am grateful for" />
 
-      <h3>3 things that would make today great</h3>
-      <PageFormItem leadText="Today would be great if" />
-      <PageFormItem leadText="Today would be great if" />
-      <PageFormItem leadText="Today would be great if" />
+        <h3>3 things that would make today great</h3>
+        <PageFormItem leadText="Today would be great if" />
+        <PageFormItem leadText="Today would be great if" />
+        <PageFormItem leadText="Today would be great if" />
 
-      <h3>Daily affirmation</h3>
-      <PageFormItem leadText="I am" />
+        <h3>Daily affirmation</h3>
+        <PageFormItem leadText="I am" />
+      </div>
+      <div className="EveningForm">
+        <h2>Evening section</h2>
 
-      <h2>Evening section</h2>
+        <h3>List 3 things that made your day amazing</h3>
+        <PageFormItem leadText="What made today amazing was" />
+        <PageFormItem leadText="What made today amazing was" />
+        <PageFormItem leadText="What made today amazing was" />
 
-      <h3>List 3 things that made your day amazing</h3>
-      <PageFormItem leadText="What made today amazing was" />
-      <PageFormItem leadText="What made today amazing was" />
-      <PageFormItem leadText="What made today amazing was" />
-
-      <h3>How could I have made today even better?</h3>
-      <PageFormItem leadText="Today would have been even better if" />
+        <h3>How could I have made today even better?</h3>
+        <PageFormItem leadText="Today would have been even better if" />
+      </div>
     </div>
   );
 };
