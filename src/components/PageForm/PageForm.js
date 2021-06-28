51e962e90f0fbd5ae2156c7e774e3c4d5d4d84fc
Melinda Dothan <melinda.dothan@gmail.com>
2021-06-28 14:20:44 +0200

    Created state for grateful statements

--- src/components/PageForm/PageForm.js
+++ src/components/PageForm/PageForm.js
@@ -7,6 +7,14 @@ const PageForm = () => {
   const [grateful1, setGrateful1] = useState("");
   const [grateful2, setGrateful2] = useState("");
   const [grateful3, setGrateful3] = useState("");
+  const [grateful4, setGrateful4] = useState("");
+  const [grateful5, setGrateful5] = useState("");
+  const [grateful6, setGrateful6] = useState("");
+  const [grateful7, setGrateful7] = useState("");
+  const [grateful8, setGrateful8] = useState("");
+  const [grateful9, setGrateful9] = useState("");
+  const [grateful10, setGrateful10] = useState("");
+  const [grateful11, setGrateful11] = useState("");
   return (
     <div className="PageForm">
       <section className="MorningForm">
@@ -31,11 +39,27 @@ const PageForm = () => {
           />
 
           <h3>3 things that would make today great</h3>
-          <PageFormItem leadText="Today would be great if" />
-          <PageFormItem leadText="Today would be great if" />
-          <PageFormItem leadText="Today would be great if" />
+          <PageFormItem
+            leadText="Today would be great if"
+            formItemValue={grateful4}
+            setFormItemValue={setGrateful4}
+          />
+          <PageFormItem
+            leadText="Today would be great if"
+            formItemValue={grateful5}
+            setFormItemValue={setGrateful5}
+          />
+          <PageFormItem
+            leadText="Today would be great if"
+            formItemValue={grateful6}
+            setFormItemValue={setGrateful6}
+          />
           <h3>Daily affirmation</h3>
-          <PageFormItem leadText="I am" />
+          <PageFormItem
+            leadText="I am"
+            formItemValue={grateful7}
+            setFormItemValue={setGrateful7}
+          />
         </div>
       </section>
       <section className="EveningForm">
@@ -43,12 +67,28 @@ const PageForm = () => {
           <h2>Evening</h2>
 
           <h3>List 3 things that made your day amazing</h3>
-          <PageFormItem leadText="What made today amazing was" />
-          <PageFormItem leadText="What made today amazing was" />
-          <PageFormItem leadText="What made today amazing was" />
+          <PageFormItem
+            leadText="What made today amazing was"
+            formItemValue={grateful8}
+            setFormItemValue={setGrateful8}
+          />
+          <PageFormItem
+            leadText="What made today amazing was"
+            formItemValue={grateful9}
+            setFormItemValue={setGrateful9}
+          />
+          <PageFormItem
+            leadText="What made today amazing was"
+            formItemValue={grateful10}
+            setFormItemValue={setGrateful10}
+          />
 
           <h3>How could I have made today even better?</h3>
-          <PageFormItem leadText="Today would have been even better if" />
+          <PageFormItem
+            leadText="Today would have been even better if"
+            formItemValue={grateful11}
+            setFormItemValue={setGrateful11}
+          />
         </div>
       </section>
     </div>
