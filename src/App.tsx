9bf81bdbdca09260c729450fcedcbe00ef9dbd0a
Nashmil <nse@leapwork.com>
2022-07-26 19:39:07 +0200

    added header and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
-const App = () => <div></div>;
+import "./App.css";
+
+const App = () => (
+  <div className="app">
+    <header className="app-header"></header>
+    <main className="app-main"></main>
+  </div>
+);
 
 export default App;
